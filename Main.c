aef49d25f01aaebb0da90a8eb121d7824c23c396
maxarasta <bdnmaxime@gmail.com>
2022-10-06 12:09:07 +0200

    Test Initial

---
+++ Main.c
@@ -0,0 +1,19 @@
+/*
+ ============================================================================
+ Name        : c_hello_world.c
+ Author      :
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+int main(void) {
+	printf("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
